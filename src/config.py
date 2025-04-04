f7c7f344454ef8a4b72f0a779e6aeb8553d80c52
JohannesKersting <kerstingjohannes@gmail.com>
2025-04-04 08:52:36 +0200

    prepared seed set files

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+
+DATA_DIR = "../data"
+
+SEED_SETS = [
+    {"name": "Amyotrophic lateral sclerosis", "short": "ALS", "mondo": "0004976", "disgenet": "C0002736"},
+    {"name": "Lung adenocarcinoma", "short": "LUAD", "mondo": "0005061", "disgenet": "C0152013"},
+    {"name": "Ulcerative colitis", "short": "UC", "mondo": "0005101", "disgenet": "C0009324"},
+    {"name": "Crohn disease", "short": "CD", "mondo": "0005011", "disgenet": "C0156147"},
+    {"name": "Huntington disease", "short": "HD", "mondo": "0007739", "disgenet": "C0020179"},
+]
